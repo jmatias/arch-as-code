64eb8e62eb361a7571b646e13b148c029b3142dc
nahknarmi <ikhan@thoughtworks.com>
2020-02-11 23:05:26 -0500

    Refactoring

--- src/main/java/net/nahknarmi/arch/transformation/enhancer/ModelEnhancer.java
+++ src/main/java/net/nahknarmi/arch/transformation/enhancer/ModelEnhancer.java
@@ -2,31 +2,34 @@ package net.nahknarmi.arch.transformation.enhancer;
 
 import com.structurizr.Workspace;
 import com.structurizr.model.*;
+import io.vavr.Tuple2;
 import net.nahknarmi.arch.domain.ArchitectureDataStructure;
-import net.nahknarmi.arch.domain.c4.C4Action;
-import net.nahknarmi.arch.domain.c4.C4Model;
-import net.nahknarmi.arch.domain.c4.C4Type;
+import net.nahknarmi.arch.domain.c4.*;
 import net.nahknarmi.arch.domain.c4.view.ModelMediator;
 import net.nahknarmi.arch.generator.PathIdGenerator;
 
+import java.util.Set;
+import java.util.function.Function;
+
 import static java.util.Optional.ofNullable;
+import static net.nahknarmi.arch.domain.c4.C4Action.DELIVERS;
+import static net.nahknarmi.arch.domain.c4.C4Action.INTERACTS_WITH;
 import static net.nahknarmi.arch.domain.c4.C4Model.NONE;
 
 public class ModelEnhancer implements WorkspaceEnhancer {
 
     public void enhance(Workspace workspace, ArchitectureDataStructure dataStructure) {
         Model workspaceModel = workspace.getModel();
-        ModelMediator modelMediator = new ModelMediator(workspaceModel);
-
         C4Model dataStructureModel = dataStructure.getModel();
-
         workspaceModel.setIdGenerator(new PathIdGenerator(dataStructureModel));
+
+        ModelMediator modelMediator = new ModelMediator(workspaceModel);
 
         addPeople(dataStructureModel, modelMediator);
         addSystems(dataStructureModel, modelMediator);
         addContainers(dataStructureModel, modelMediator);
-        addComponents(dataStructureModel, new ModelMediator(workspaceModel));
-        addRelationships(workspaceModel, dataStructureModel);
+        addComponents(dataStructureModel, modelMediator);
+        addRelationships(dataStructureModel, modelMediator);
     }
 
     private void addPeople(C4Model dataStructureModel, ModelMediator modelMediator) {
@@ -51,204 +54,83 @@ public class ModelEnhancer implements WorkspaceEnhancer {
         dataStructureModel.getComponents().forEach(modelMediator::addComponent);
     }
 
-    private void addRelationships(Model workspaceModel, C4Model dataStructureModel) {
-        addPeopleRelationships(workspaceModel, dataStructureModel);
-        addSystemRelationships(workspaceModel, dataStructureModel);
-        addContainerRelationships(workspaceModel, dataStructureModel);
-        addComponentRelationships(workspaceModel, dataStructureModel);
+    private void addRelationships(C4Model dataStructureModel, ModelMediator modelMediator) {
+        addPeopleRelationships(modelMediator, dataStructureModel.getPeople(), x -> new Tuple2<>(x, modelMediator.person(x.getPath())));
+        addNonPersonRelationships(modelMediator, dataStructureModel.getSystems(), x -> new Tuple2<>(x, modelMediator.softwareSystem(x.getPath())));
+        addNonPersonRelationships(modelMediator, dataStructureModel.getContainers(), x -> new Tuple2<>(x, modelMediator.container(x.getPath())));
+        addNonPersonRelationships(modelMediator, dataStructureModel.getComponents(), x -> new Tuple2<>(x, modelMediator.component(x.getPath())));
     }
 
-    private void addPeopleRelationships(Model workspaceModel, C4Model dataStructureModel) {
-        ModelMediator modelMediator = new ModelMediator(workspaceModel);
-
-        dataStructureModel.getPeople().forEach(p -> {
-            Person person = modelMediator.person(p.getPath());
-
-            p.getRelationships()
-                    .forEach(r -> {
-                        String description = r.getDescription();
-                        String technology = r.getTechnology();
-                        C4Type typeDestination = r.getWith().type();
-
-                        if (r.getAction() == C4Action.USES) {
-                            SoftwareSystem systemDestination = modelMediator.softwareSystem(r.getWith());
-
-                            switch (typeDestination) {
-                                case system: {
-                                    person.uses(systemDestination, description, technology);
-                                    break;
-                                }
-                                case container: {
-                                    Container container = modelMediator.container(r.getWith());
-                                    person.uses(container, description, technology);
-                                    break;
-                                }
-                                case component: {
-                                    Component component = modelMediator.component(r.getWith());
-                                    person.uses(component, description, technology);
-                                    break;
-                                }
-                                default:
-                                    throw new IllegalStateException("Unsupported type " + typeDestination);
-                            }
-                        }
-                        if (r.getAction() == C4Action.INTERACTS_WITH) {
-                            if (typeDestination != C4Type.person) {
-                                throw new IllegalStateException("Action INTERACTS_WITH supported only with type person, not: " + typeDestination);
-                            } else {
-                                Person personDestination = modelMediator.person(r.getWith());
-                                person.interactsWith(personDestination, description, technology);
-                            }
-                        }
-                    });
-        });
+    private void addPeopleRelationships(ModelMediator modelMediator, Set<? extends BaseEntity> entities, Function<BaseEntity, ? extends Tuple2<? extends BaseEntity, StaticStructureElement>> tuple2Function) {
+        entities.stream().map(tuple2Function)
+                .forEach(tuple2 ->
+                        tuple2._1()
+                                .getRelationships()
+                                .forEach(r -> {
+                                    addUsesRelationship(modelMediator, tuple2._2(), r);
+                                    addInteractsWith(modelMediator, (Person) tuple2._2(), r);
+                                })
+                );
     }
 
-    private void addSystemRelationships(Model workspaceModel, C4Model dataStructureModel) {
-        ModelMediator modelMediator = new ModelMediator(workspaceModel);
-        dataStructureModel.getSystems().forEach(s -> {
-            SoftwareSystem softwareSystem = modelMediator.softwareSystem(s.getPath());
-
-            s.getRelationships()
-                    .forEach(r -> {
-                        String description = r.getDescription();
-                        String technology = r.getTechnology();
-                        C4Type typeDestination = r.getWith().type();
-
-                        if (r.getAction() == C4Action.USES) {
-                            SoftwareSystem systemDestination = modelMediator.softwareSystem(r.getWith());
-
-                            switch (typeDestination) {
-                                case system: {
-                                    softwareSystem.uses(systemDestination, description, technology);
-                                    break;
-                                }
-                                case container: {
-                                    Container containerDestination = modelMediator.container(r.getWith());
-                                    softwareSystem.uses(containerDestination, description, technology);
-                                    break;
-                                }
-                                case component: {
-                                    Component component = modelMediator.component(r.getWith());
-                                    softwareSystem.uses(component, description, technology);
-                                    break;
-                                }
-                                default:
-                                    throw new IllegalStateException("Unsupported type " + typeDestination);
-                            }
-                        }
-                        if (r.getAction() == C4Action.DELIVERS) {
-                            if (typeDestination != C4Type.person) {
-                                throw new IllegalStateException("Action DELIVERS supported only with type person, not: " + typeDestination);
-                            } else {
-                                Person personDestination = modelMediator.person(r.getWith());
-                                softwareSystem.delivers(personDestination, description, technology);
-                            }
-                        }
-                    });
-        });
+    private void addNonPersonRelationships(ModelMediator modelMediator, Set<? extends BaseEntity> entities, Function<BaseEntity, ? extends Tuple2<? extends BaseEntity, StaticStructureElement>> tuple2Function) {
+        entities.stream()
+                .map(tuple2Function)
+                .forEach(tuple2 -> addUsesAndDeliversRelations(modelMediator, tuple2._1(), tuple2._2()));
     }
 
-    private void addContainerRelationships(Model workspaceModel, C4Model dataStructureModel) {
-        ModelMediator modelMediator = new ModelMediator(workspaceModel);
-
-        dataStructureModel.getContainers().forEach(c -> {
-            Container container = modelMediator.container(c.getPath());
-
-            c.getRelationships()
-                    .forEach(r -> {
-                        String description = r.getDescription();
-                        String technology = r.getTechnology();
-                        C4Type typeDestination = r.getWith().type();
-
-                        if (r.getAction() == C4Action.USES) {
-                            SoftwareSystem systemDestination = modelMediator.softwareSystem(r.getWith());
-
-                            switch (typeDestination) {
-                                case system: {
-                                    container.uses(systemDestination, description, technology);
-                                    break;
-                                }
-                                case container: {
-                                    Container containerDestination = modelMediator.container(r.getWith());
-                                    container.uses(containerDestination, description, technology);
-                                    break;
-                                }
-                                case component: {
-                                    Component componentDestination = modelMediator.component(r.getWith());
-
-                                    if (componentDestination == null) {
-                                        System.err.println("Hanging reference - " + r.getWith());
-                                    } else {
-                                        container.uses(componentDestination, description, technology);
-                                    }
-
-                                    break;
-                                }
-                                default:
-                                    throw new IllegalStateException("Unsupported type " + typeDestination);
-                            }
-                        }
-                        if (r.getAction() == C4Action.DELIVERS) {
-                            if (typeDestination != C4Type.person) {
-                                throw new IllegalStateException("Action DELIVERS supported only with type person, not: " + typeDestination);
-                            } else {
-                                Person person = modelMediator.person(r.getWith());
-                                container.delivers(person, description, technology);
-                            }
-                        }
-                    });
-        });
+    private void addUsesAndDeliversRelations(ModelMediator modelMediator, HasRelation c, StaticStructureElement container) {
+        c.getRelationships()
+                .forEach(r -> {
+                    addUsesRelationship(modelMediator, container, r);
+                    addDelivers(modelMediator, container, r);
+                });
     }
 
-    private void addComponentRelationships(Model workspaceModel, C4Model dataStructureModel) {
-        ModelMediator modelMediator = new ModelMediator(workspaceModel);
-        dataStructureModel.getComponents().forEach(comp -> {
-            Component component = modelMediator.component(comp.getPath());
+    private void addUsesRelationship(ModelMediator modelMediator, StaticStructureElement element, C4Relationship r) {
+        if (C4Action.USES.equals(r.getAction())) {
+            SoftwareSystem systemDestination = modelMediator.softwareSystem(r.getWith());
 
-            comp.getRelationships()
-                    .forEach(r -> {
-                        String description = r.getDescription();
-                        String technology = r.getTechnology();
-                        C4Type typeDestination = r.getWith().type();
+            switch (r.getWith().type()) {
+                case system: {
+                    element.uses(systemDestination, r.getDescription(), r.getTechnology());
+                    break;
+                }
+                case container: {
+                    Container containerDestination = modelMediator.container(r.getWith());
+                    element.uses(containerDestination, r.getDescription(), r.getTechnology());
+                    break;
+                }
+                case component: {
+                    Component component = modelMediator.component(r.getWith());
+                    element.uses(component, r.getDescription(), r.getTechnology());
+                    break;
+                }
+                default:
+                    throw new IllegalStateException("Unsupported type " + r.getWith().type());
+            }
+        }
+    }
 
-                        if (r.getAction() == C4Action.USES) {
-                            SoftwareSystem systemDestination = modelMediator.softwareSystem(r.getWith());
+    private void addDelivers(ModelMediator modelMediator, StaticStructureElement element, C4Relationship r) {
+        if (DELIVERS.equals(r.getAction())) {
+            if (!C4Type.person.equals(r.getWith().type())) {
+                throw new IllegalStateException("Action DELIVERS supported only with type person, not: " + r.getWith().type());
+            } else {
+                Person person = modelMediator.person(r.getWith());
+                element.delivers(person, r.getDescription(), r.getTechnology());
+            }
+        }
+    }
 
-                            switch (typeDestination) {
-                                case system: {
-                                    component.uses(systemDestination, description, technology);
-                                    break;
-                                }
-                                case container: {
-                                    Container containerDestination = modelMediator.container(r.getWith());
-                                    component.uses(containerDestination, description, technology);
-                                    break;
-                                }
-                                case component: {
-                                    Component componentDestination = modelMediator.component(r.getWith());
-
-                                    if (componentDestination == null) {
-                                        System.err.println("Hanging reference " + r.getWith());
-                                    } else {
-                                        component.uses(componentDestination, description, technology);
-                                    }
-
-                                    break;
-                                }
-                                default:
-                                    throw new IllegalStateException("Unsupported type " + typeDestination);
-                            }
-                        }
-                        if (r.getAction() == C4Action.DELIVERS) {
-                            if (typeDestination != C4Type.person) {
-                                throw new IllegalStateException("Action DELIVERS supported only with type person, not: " + typeDestination);
-                            } else {
-                                Person person = modelMediator.person(r.getWith());
-                                component.delivers(person, description, technology);
-                            }
-                        }
-                    });
-        });
+    private void addInteractsWith(ModelMediator modelMediator, Person person, C4Relationship r) {
+        if (INTERACTS_WITH.equals(r.getAction())) {
+            if (!C4Type.person.equals(r.getWith().type())) {
+                throw new IllegalStateException("Action INTERACTS_WITH supported only with type person, not: " + r.getWith().type());
+            } else {
+                Person personDestination = modelMediator.person(r.getWith());
+                person.interactsWith(personDestination, r.getDescription(), r.getTechnology());
+            }
+        }
     }
 }
